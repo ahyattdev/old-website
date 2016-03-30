e2c66768b2233d2c360f5809b30dd376a863dae7
Andrew Hyatt <andythehyatt@gmail.com>
2016-03-29 17:17:16 -0700

    Put fix in the right place

--- projects/radio-op/scripts/fix.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("game").children[0].style.border = "0px";
--- projects/radio-op/scripts/load.js
+++ projects/radio-op/scripts/load.js
@@ -5,6 +5,7 @@ var load = {
     },
     
     create: function() {
+        document.getElementById("game").children[0].style.border = "0px";
         game.state.start("menu");
     }
     
