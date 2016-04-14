dd900c3d8a50cfcfb4f355f27105428cabc9fe9b
Andrew Hyatt <ahyattdev@icloud.com>
2016-04-13 23:03:07 -0700

    Completed Pearl Harbor transcript

--- projects/radio-op/scripts/pearl-harbor.js
+++ projects/radio-op/scripts/pearl-harbor.js
@@ -5,7 +5,11 @@ function PearlHarbor() {
     this.TIME = "11 AM Pacific Time, 8 AM Hawaii Time"
     this.transcriptData = [
         ["Air raid Pearl Harbor", this.time],
-        ["This is not drill", this.time]
+        ["This is not drill", this.time],
+        ["The Japanese have attacked", this.time],
+        ["Alert the president", this.time],
+        ["This is not drill", this.time],
+        ["Get this to Washington", this.time]
     ];
     
     this.sucess = "Congratulations, you have sucessfully simulated the experience of a radio operator during the attack on Pearl Harbor"
