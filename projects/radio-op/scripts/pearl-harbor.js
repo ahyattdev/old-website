0c91a3c86d8f27d28163e33f757639cfc8304ee6
Andrew Hyatt <ahyattdev@icloud.com>
2016-04-15 10:34:19 -0700

    Fixed spelling error

--- projects/radio-op/scripts/pearl-harbor.js
+++ projects/radio-op/scripts/pearl-harbor.js
@@ -5,10 +5,10 @@ function PearlHarbor() {
     this.TIME = "11 AM Pacific Time, 8 AM Hawaii Time"
     this.transcriptData = [
         ["Air raid Pearl Harbor", this.TIME],
-        ["This is not drill", this.TIME],
+        ["This is not a drill", this.TIME],
         ["The Japanese have attacked", this.TIME],
         ["Alert the president", this.TIME],
-        ["This is not drill", this.TIME],
+        ["This is not a drill", this.TIME],
         ["Get this to Washington", this.TIME]
     ];
     
