4d37031ad49c5a9e0ddca898086bf41c71ce8255
Andrew Hyatt <ahyattdev@icloud.com>
2016-07-21 08:12:20 -0700

    Create navbar and footer

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("nav").innerHTML =
-`
-<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/projects">Projects</a></li>
-</ul>
-`;
