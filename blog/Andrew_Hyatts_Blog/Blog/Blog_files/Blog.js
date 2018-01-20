ce1a4bb96f46d2559a962ec1622f89c836a91d65
Andrew Hyatt <ahyattdev@icloud.com>
2018-01-20 12:05:07 -0800

    Create blog
    Signed-off-by: Andrew Hyatt <ahyattdev@icloud.com>

---
+++ blog/Andrew_Hyatts_Blog/Blog/Blog_files/Blog.js
@@ -0,0 +1,13 @@
+// Created by iWeb 3.0.2 local-build-20180120
+
+setTransparentGifURL('../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("https://ahyattdev.github.io/blog/Andrew_Hyatts_Blog/Blog/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Blog_files/BlogMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id3');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ blog/Andrew_Hyatts_Blog/Blog/Entries/2018/1/20_An_alternative_to_caffeinate_on_Leopard_files/20_An_alternative_to_caffeinate_on_Leopard.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20180120
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('20_An_alternative_to_caffeinate_on_Leopard_files/20_An_alternative_to_caffeinate_on_LeopardMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id4');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
